835f4988c9457c4061bbcf0fd1eacc0a7685aa1b
HE CHENG <as0960311360@gmail.com>
2023-06-16 15:56:03 +0800

    avg count version

--- review_analytics.py
+++ review_analytics.py
@@ -7,5 +7,9 @@ with open ("reviews.txt","r") as f :
         if count % 1000 == 0 :
             print(len(data))
 
-print(len(data))
-print(data[0])
+sum_len = 0
+with open ("reviews.txt","r") as f :
+    for line in f :
+        data.append(line)
+        sum_len = sum_len + len(line)
+print('平均留言長度為：', sum_len / len(data) )
